f8b9d5206bf9d904eeaa73c69488c7d1b86e2bf7
yizhe-ang <ang.yizhe@u.nus.edu>
2024-08-02 18:42:52 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Scene from "./components/Scene";
 import useDepthEstimator from "./lib/useDepthEstimator";
 
 // FIXME: Image resolution is also a performance bottleneck
-const img = "watercolor_1.jpeg";
+const img = "/watercolor_1.jpeg";
 
 function App() {
   // Perform depth estimation
